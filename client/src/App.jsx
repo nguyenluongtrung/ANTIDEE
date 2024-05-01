675cc0f78fb186e6d2a204ec990542abaf1d33ca
Kenn1412 <baohungdinhnguyen1412@gmail.com>
2024-05-01 23:23:40 +0700

    AN-94 "Create Admin UI"

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,27 @@
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import { Layout } from './layout';
-import { LoginPage } from './pages/LoginPage';
-import { RegisterPage } from './pages/RegisterPage';
-import { HomePage } from './pages/HomePage';
-import { MyAccount } from './pages/MyAccount';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Layout } from "./layout";
+import { LoginPage } from "./pages/LoginPage";
+import { RegisterPage } from "./pages/RegisterPage";
+import { HomePage } from "./pages/HomePage";
+import { MyAccount } from "./pages/MyAccount";
+import { AdminPage } from "./pages/AdminPage";
 
 const App = () => {
   return (
     <div>
       <Router>
         <Routes>
-          <Route path='/' element={<Layout />}>
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/home" element={<HomePage />} />
-          <Route path="/my-account" element={<MyAccount />} />
+          <Route path="/" element={<Layout />}>
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/my-account" element={<MyAccount />} />
+            <Route path="/admin" element={<AdminPage />} />
           </Route>
         </Routes>
       </Router>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
