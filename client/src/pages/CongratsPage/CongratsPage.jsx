904578bc17f3b2a99d6112529014051e19687453
nguyenluongtrung <100284665+nguyenluongtrung@users.noreply.github.com>
2024-05-02 11:11:27 +0700

    Update congrats page

--- client/src/pages/CongratsPage/CongratsPage.jsx
+++ client/src/pages/CongratsPage/CongratsPage.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import img31 from '../../assets/img/image 31.png';    
+import img31 from '../../assets/img/image 31.png';
 export const CongratsPage = () => {
-    
-        return (
-            <div >
-                <img className='m-auto' src={img31} />
-                <h1 className=' grid text-green font-bold text-2xl justify-center '>CHÚC MỪNG!</h1>
-                <span className='grid justify-center text-sm font-medium'>Bạn đã chính thức trờ thành người giúp việc chuyên nghiệp</span>
+	return (
+		<div>
+			<img className="m-auto" src={img31} />
+			<h1 className=" grid text-green font-bold text-2xl justify-center pb-3">
+				CHÚC MỪNG!
+			</h1>
+			<span className="grid justify-center text-sm font-medium text-gray pb-3">
+				Bạn đã chính thức trờ thành người giúp việc chuyên nghiệp
+			</span>
 
-                        <div className='grid justify-center p-3'>
-                        <button className='bg-primary text-white w-max py-2 px-24 rounded-full'>Khám phá những tính năng của người giúp việc</button>
-                    </div>
-                </div>
-        );
-    };
-    
+			<div className="grid justify-center p-3">
+				<button className="bg-primary text-white w-max py-2 px-24 rounded-full">
+					Khám phá những tính năng của người giúp việc
+				</button>
+			</div>
+		</div>
+	);
+};
