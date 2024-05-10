678505b798da10ca0134f926a28cede415935a32
nguyenluongtrung <100284665+nguyenluongtrung@users.noreply.github.com>
2024-05-10 21:52:29 +0700

    Update validation in my account function

--- client/src/utils/rules.js
+++ client/src/utils/rules.js
@@ -4,11 +4,19 @@ export const rules = {
 			value: /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/,
 			message: 'Số điện thoại không hợp lệ',
 		},
+		required: {
+			value: true,
+			message: 'Số điện thoại là bắt buộc',
+		},
 	},
 	email: {
 		pattern: {
 			value: /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6})*$/,
 			message: 'Email không hợp lệ',
+		},
+		required: {
+			value: true,
+			message: 'Email là bắt buộc',
 		},
 	},
 	password: {
@@ -19,6 +27,18 @@ export const rules = {
 		minLength: {
 			value: 8,
 			message: 'Mật khẩu phải chứa ít nhất 8 kí tự',
+		},
+	},
+	name: {
+		required: {
+			value: true,
+			message: 'Tên là bắt buộc',
+		},
+	},
+	dob: {
+		required: {
+			value: true,
+			message: 'Ngày sinh là bắt buộc',
 		},
 	},
 };
