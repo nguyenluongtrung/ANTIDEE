0919561c99896d173c113c9833d01d5e6c2302c0
nguyenluongtrung <100284665+nguyenluongtrung@users.noreply.github.com>
2024-05-30 11:02:32 +0700

    Filter jobs in registered location

--- client/src/pages/JobPostListPage/JobPostListPage.jsx
+++ client/src/pages/JobPostListPage/JobPostListPage.jsx
@@ -4,13 +4,26 @@ import { Spinner } from '../../components';
 import { getAllJobPosts } from '../../features/jobPosts/jobPostsSlice';
 import { formatDate, formatTime, formatWorkingTime } from '../../utils/format';
 import { JobPostDetail } from './JobPostDetail/JobPostDetail';
+import { getAccountInformation } from '../../features/auth/authSlice';
 
 export const JobPostListPage = () => {
 	const [isOpenJobPostDetail, setIsOpenJobPostDetail] = useState(false);
 	const [chosenJobPostId, setChosenJobPostId] = useState(null);
+	const [isInMyLocation, setIsInMyLocation] = useState(false);
+	const [account, setAccount] = useState();
 
 	const dispatch = useDispatch();
 	const { jobPosts, isLoading } = useSelector((state) => state.jobPosts);
+
+	async function initiateAccountInformation() {
+		let output = await dispatch(getAccountInformation());
+
+		setAccount(output.payload);
+	}
+
+	useEffect(() => {
+		initiateAccountInformation();
+	}, []);
 
 	useEffect(() => {
 		dispatch(getAllJobPosts());
@@ -51,50 +64,47 @@ export const JobPostListPage = () => {
 					</div>
 				</div>
 				<div className="flex">
-					<input type="checkbox" className="w-3 mr-2" />
+					<input
+						type="checkbox"
+						className="w-3 mr-2"
+						onClick={() => setIsInMyLocation(!isInMyLocation)}
+					/>
 					<p className="font-bold">Lọc công việc trong khu vực đã đăng ký</p>
 				</div>
 			</div>
 			<div className="grid grid-cols-3 gap-28">
-				{jobPosts?.filter((jobPost) => jobPost.domesticHelperId == null)?.map((post) => {
-					return (
-						<div className="shadow-xl p-7 hover:shadow-2xl hover:cursor-pointer relative" style={{height: '350px'}}>
-							<p className="text-brown font-bold mb-3">
-								{post?.serviceId?.name?.toUpperCase()}
-							</p>
-							<p className="text-gray mb-2">
-								Bắt đầu lúc:{' '}
-								<span className="text-brown">
-									{formatDate(post?.workingTime?.startingDate)}{' '}
-									{formatWorkingTime(post?.workingTime?.startingHour)}
-								</span>
-							</p>
-							<div className="border-2 border-gray  my-3">
-								{post?.workload?.find(
-									(option) => String(option?.optionName) === 'Thời gian'
-								)?.optionValue == undefined ? (
-									<div>
-										<p className="text-gray mb-2 text-center mt-3">Số tiền: </p>
-										<p className="text-center text-brown font-bold mb-3">
-											{post?.totalPrice} VND
-										</p>
-									</div>
-								) : (
-									<div className="grid grid-cols-2">
-										<div className="border-r-2 border-gray">
-											<p className="text-gray mb-2 text-center mt-3">
-												Làm trong:{' '}
-											</p>
-											<p className="text-center text-brown font-bold mb-3">
-												{
-													post?.workload?.find(
-														(option) =>
-															String(option?.optionName) === 'Thời gian'
-													)?.optionValue
-												}{' '}
-												giờ
-											</p>
-										</div>
+				{jobPosts
+					?.filter((jobPost) => {
+						if(isInMyLocation){
+							const addressDetails = jobPost?.contactInfo?.address.split(",");
+							const jobCity = addressDetails[addressDetails.length - 1].trim();
+							const myCity = account?.address.split(",")[account?.address.split(",").length - 1].trim();
+							return jobCity.toUpperCase().includes(myCity.toUpperCase())
+						} else{
+							return jobPost;
+						}
+					})
+					?.filter((jobPost) => jobPost.domesticHelperId == null)
+					?.map((post) => {
+						return (
+							<div
+								className="shadow-xl p-7 hover:shadow-2xl hover:cursor-pointer relative"
+								style={{ height: '350px' }}
+							>
+								<p className="text-brown font-bold mb-3">
+									{post?.serviceId?.name?.toUpperCase()}
+								</p>
+								<p className="text-gray mb-2">
+									Bắt đầu lúc:{' '}
+									<span className="text-brown">
+										{formatDate(post?.workingTime?.startingDate)}{' '}
+										{formatWorkingTime(post?.workingTime?.startingHour)}
+									</span>
+								</p>
+								<div className="border-2 border-gray  my-3">
+									{post?.workload?.find(
+										(option) => String(option?.optionName) === 'Thời gian'
+									)?.optionValue == undefined ? (
 										<div>
 											<p className="text-gray mb-2 text-center mt-3">
 												Số tiền:{' '}
@@ -103,34 +113,60 @@ export const JobPostListPage = () => {
 												{post?.totalPrice} VND
 											</p>
 										</div>
-									</div>
-								)}
+									) : (
+										<div className="grid grid-cols-2">
+											<div className="border-r-2 border-gray">
+												<p className="text-gray mb-2 text-center mt-3">
+													Làm trong:{' '}
+												</p>
+												<p className="text-center text-brown font-bold mb-3">
+													{
+														post?.workload?.find(
+															(option) =>
+																String(option?.optionName) === 'Thời gian'
+														)?.optionValue
+													}{' '}
+													giờ
+												</p>
+											</div>
+											<div>
+												<p className="text-gray mb-2 text-center mt-3">
+													Số tiền:{' '}
+												</p>
+												<p className="text-center text-brown font-bold mb-3">
+													{post?.totalPrice} VND
+												</p>
+											</div>
+										</div>
+									)}
+								</div>
+								<p className="text-gray mb-2 ">
+									Tại:{' '}
+									<span className="text-black">
+										{post?.contactInfo?.address}
+									</span>
+								</p>
+								<p className="text-gray mb-3">
+									Ghi chú:{' '}
+									<span className="text-black">
+										{post?.note ? post?.note : 'Không có'}
+									</span>
+								</p>
+								<div className="flex flex-col items-center absolute bottom-5 left-12 w-72">
+									<button
+										className="mt-5 text-white bg-brown rounded-2xl text-xs py-2.5 text-center hover:bg-light_yellow hover:text-brown"
+										style={{ width: '70%' }}
+										onClick={() => {
+											setIsOpenJobPostDetail(true);
+											setChosenJobPostId(post?._id);
+										}}
+									>
+										<p className="text-center">Xem chi tiết công việc</p>
+									</button>
+								</div>
 							</div>
-							<p className="text-gray mb-2 ">
-								Tại:{' '}
-								<span className="text-black">{post?.contactInfo?.address}</span>
-							</p>
-							<p className="text-gray mb-3">
-								Ghi chú:{' '}
-								<span className="text-black">
-									{post?.note ? post?.note : 'Không có'}
-								</span>
-							</p>
-							<div className="flex flex-col items-center absolute bottom-5 left-12 w-72">
-								<button
-									className="mt-5 text-white bg-brown rounded-2xl text-xs py-2.5 text-center hover:bg-light_yellow hover:text-brown"
-									style={{ width: '70%' }}
-									onClick={() => {
-										setIsOpenJobPostDetail(true);
-										setChosenJobPostId(post?._id);
-									}}
-								>
-									<p className="text-center">Xem chi tiết công việc</p>
-								</button>
-							</div>
-						</div>
-					);
-				})}
+						);
+					})}
 			</div>
 		</div>
 	);
