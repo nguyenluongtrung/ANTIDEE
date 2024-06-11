9ca861ea8f54b73fe7a3ec7e199eef61b6a9387c
buivangiaphatdev <buivangiaphat@gmail.com>
2024-06-11 21:31:26 +0700

    service carouel UI

--- client/src/pages/HomePage/HomePage.jsx
+++ client/src/pages/HomePage/HomePage.jsx
@@ -1,51 +1,52 @@
-import React, { useEffect, useRef, useState } from 'react';
-import './HomePage.css';
-import { FiSearch } from 'react-icons/fi';
-import { SlArrowRight } from 'react-icons/sl';
-import { SlArrowLeft } from 'react-icons/sl';
-import { useDispatch, useSelector } from 'react-redux';
-import { Spinner } from '../../components';
-import { getAllServices } from '../../features/services/serviceSlice';
-import { useNavigate } from 'react-router-dom';
-import { useTypewriter } from 'react-simple-typewriter';
-import { FaArrowUp } from 'react-icons/fa';
+import React, { useEffect, useRef, useState } from "react";
+import "./HomePage.css";
+import { FiSearch } from "react-icons/fi";
+import { SlArrowRight } from "react-icons/sl";
+import { SlArrowLeft } from "react-icons/sl";
+import { useDispatch, useSelector } from "react-redux";
+import { Spinner } from "../../components";
+import { getAllServices } from "../../features/services/serviceSlice";
+import { useNavigate } from "react-router-dom";
+import { useTypewriter } from "react-simple-typewriter";
+import { FaArrowUp } from "react-icons/fa";
+
+
 export const HomePage = () => {
 	const [services, setServices] = useState([]);
 	const topRef = useRef();
-	const [currentIndex, setCurrentIndex] = useState(0);
 	const { isLoading } = useSelector((state) => state.services);
 	const dispatch = useDispatch();
 	const navigate = useNavigate();
 	const benefit = [
 		{
-			name: 'Đặt lịch nhanh chóng',
-			image: 'image/time.png',
-			text: 'Thao tác 60 giây trên ứng dụng, có ngay người giúp việc sau 60 phút',
+			name: "Đặt lịch nhanh chóng",
+			image: "image/time.png",
+			text: "Thao tác 60 giây trên ứng dụng, có ngay người giúp việc sau 60 phút",
 		},
 		{
-			name: 'Giá cả rõ ràng',
-			image: 'image/money.png',
-			text: 'Giá cả dịch vụ trên ứng dụng hiển thị rõ ràng, bạn không cần trả thêm bất kì khoản chi phí nào',
+			name: "Giá cả rõ ràng",
+			image: "image/money.png",
+			text: "Giá cả dịch vụ trên ứng dụng hiển thị rõ ràng, bạn không cần trả thêm bất kì khoản chi phí nào",
 		},
 		{
-			name: 'Đa dạng dịch vụ',
-			image: 'image/dadang.png',
-			text: 'Antidee sẵn sàng hỗ trợ mọi nhu cầu việc nhà của bạn',
+			name: "Đa dạng dịch vụ",
+			image: "image/dadang.png",
+			text: "Antidee sẵn sàng hỗ trợ mọi nhu cầu việc nhà của bạn",
 		},
 		{
-			name: 'An toàn tối đa',
-			image: 'image/baove.png',
-			text: 'Người làm uy tín, luôn có hồ sơ lý lịch rõ ràng và được công ty giám sát trong suốt quá trình làm việc ',
+			name: "An toàn tối đa",
+			image: "image/baove.png",
+			text: "Người làm uy tín, luôn có hồ sơ lý lịch rõ ràng và được công ty giám sát trong suốt quá trình làm việc ",
 		},
 	];
 	const achievement = [
-		{ name: '97%', image: 'image/nguoi.png', text: 'Khách hàng hài lòng' },
+		{ name: "97%", image: "image/nguoi.png", text: "Khách hàng hài lòng" },
 		{
-			name: '3,600,000',
-			image: 'image/tich.png',
-			text: 'Công việc được hoàn thành',
+			name: "3,600,000",
+			image: "image/tich.png",
+			text: "Công việc được hoàn thành",
 		},
-		{ name: '11,500,000+', image: 'image/dongho.png', text: 'Giờ làm việc' },
+		{ name: "11,500,000+", image: "image/dongho.png", text: "Giờ làm việc" },
 	];
 
 	async function initiateServices() {
@@ -59,17 +60,17 @@ export const HomePage = () => {
 	}, []);
 
 	const [typeEffect] = useTypewriter({
-		words: ['Việc gì khó có Antidee lo'],
+		words: ["Việc gì khó có Antidee lo"],
 		loop: {},
 		typeSpeed: 100,
 		deleteSpeed: 40,
 	});
 
 	const scrolLWithUseRef = () => {
-		topRef.current?.scrollIntoView({ block: 'center', behavior: 'smooth' });
+		topRef.current?.scrollIntoView({ block: "center", behavior: "smooth" });
 	};
 
-	const handleNextService = () => {
+	const handlePreviousService = () => {
 		setServices((prevServices) => {
 			const lastService = prevServices[prevServices.length - 1];
 			const newServices = prevServices.filter(
@@ -79,7 +80,7 @@ export const HomePage = () => {
 		});
 	};
 
-	const handlePreviousService = () => {
+	const handleNextService = () => {
 		setServices((prevServices) => {
 			const firstService = prevServices[0];
 			const newServices = prevServices.filter((_, index) => index !== 0);
@@ -91,19 +92,24 @@ export const HomePage = () => {
 		navigate(`/job-posting/view-service-detail/${id}`);
 	};
 
+
+
 	if (isLoading || !Array.isArray(services)) {
 		return <Spinner />;
 	}
 
 	return (
 		<div className="">
-			<div className="fixed mr-3 mb-10 rounded-full p-5 hover:cursor-pointer bottom-0 right-10 bg-light_purple hover:border-0 hover:opacity-80" onClick={scrolLWithUseRef}>
+			<div
+				className="fixed mr-3 mb-10 rounded-full p-5 hover:cursor-pointer bottom-0 right-10 bg-light_purple hover:border-0 hover:opacity-80"
+				onClick={scrolLWithUseRef}
+			>
 				<FaArrowUp className="text-pink" />
 			</div>
 			<div>
 				<div
 					className="bg-primary p-8 text-white  mx-auto rounded-[20px] mt-20 relative"
-					style={{ maxWidth: '1255px', height: '292px' }}
+					style={{ maxWidth: "1255px", height: "292px" }}
 					ref={topRef}
 				>
 					<p className="text-center mt-6">
@@ -112,7 +118,7 @@ export const HomePage = () => {
 
 					<div
 						className="absolute top-32 left-72 bg-white flex items-center rounded-2xl "
-						style={{ height: '70px', width: '700px' }}
+						style={{ height: "70px", width: "700px" }}
 					>
 						<input
 							type="text"
@@ -121,71 +127,86 @@ export const HomePage = () => {
 						/>
 						<button
 							className="bg-primary rounded-[15px] mr-4"
-							style={{ maxWidth: '53px', height: '53px' }}
+							style={{ maxWidth: "53px", height: "53px" }}
 						>
 							<FiSearch
 								className="mx-auto"
-								style={{ width: '33px', height: '33px' }}
+								style={{ width: "33px", height: "33px" }}
 							/>
 						</button>
 					</div>
 				</div>
 			</div>
+
 			<div className="mt-20">
 				<p
 					className="ml-[120px] mt-7"
-					style={{ color: '#562A0E', fontSize: '30px' }}
+					style={{ color: "#562A0E", fontSize: "30px" }}
 				>
 					Dịch vụ hàng đầu
 				</p>
-				<div className="flex justify-center mt-4 ml-[25px]">
-					<div className="mt-[110px]">
-						<div
-							className=" inline-block p-2 rounded-full "
-							style={{ backgroundColor: '#F9F6F6' }}
-							onClick={handlePreviousService}
-						>
-							<SlArrowLeft className="hover:cursor-pointer" />
-						</div>
-					</div>
-					{services
-						?.slice(currentIndex, currentIndex + 5)
-						.map((service, index) => (
+				{/* ================================================================ */}
+				
+				
+					<div className=" flex justify-center mt-4 ml-[25px] ">
+						{/* <div className="mt-[110px]">
 							<div
-								key={index}
-								className="mx-4 transition duration-300 ease-in-out transform hover:scale-110 border-gray-300 shadow-2xl rounded-[15px] hover:cursor-pointer"
+								className=" inline-block p-2 rounded-full "
+								style={{ backgroundColor: "#F9F6F6" }}
+								onClick={handlePreviousService}
 							>
-								<div className="p-4">
-									<p
-										className="mb-3 text-primary font-bold opacity-80 mt-1"
-										style={{ fontSize: '15px' }}
-									>
-										{service?.name}
-									</p>
-									<img
-										src={service?.image}
-										alt={service?.name}
-										className="w-[175px] h-[155px] object-cover rounded-lg "
-										onClick={() => navigateToServicePage(service?._id)}
-									/>
-								</div>
+								<SlArrowLeft className="hover:cursor-pointer" />
 							</div>
-						))}
-					<div className="mt-[110px]">
-						<div
-							className=" inline-block p-2 rounded-full"
-							style={{ backgroundColor: '#F9F6F6' }}
-							onClick={handleNextService}
-						>
-							<SlArrowRight className="hover:cursor-pointer" />
-						</div>
-					</div>
-				</div>
+						</div> */}
+						
+							<div className="flex wrapper">
+								{services.map((service, index) => (
+
+									<div className={`service service${index}`}>
+										<div
+											key={index}
+											className=" mx-4 transition duration-300
+								 						ease-in-out transform hover:scale-110
+								 							 border-gray shadow-2xl rounded-[15px] 
+								 								 hover:cursor-pointer"
+										>
+											<div className="p-4 size-64">
+												<p
+													className="mb-3 text-primary font-bold opacity-80 mt-1"
+													style={{ fontSize: "15px" }}
+												>
+													{service?.name}
+												</p>
+												<img
+													src={service?.image}
+													alt={service?.name}
+													className="w-[225px] h-[185px] object-cover rounded-lg "
+													onClick={() => navigateToServicePage(service?._id)}
+												/>
+											</div>
+										</div>
+									</div>
+
+								))}
+							</div>
+						{/* <div className="mt-[110px]">
+							<div
+								className=" inline-block p-2 rounded-full"
+								style={{ backgroundColor: "#F9F6F6" }}
+								onClick={handleNextService}
+							>
+								<SlArrowRight className="hover:cursor-pointer" />
+							</div>
+						</div> */}
+					</div>	
+				{/* ================================================================ */}
 			</div>
+
+
 			<div className="mt-20">
 				<p
 					className="ml-[120px] mt-7"
-					style={{ color: '#562A0E', fontSize: '30px' }}
+					style={{ color: "#562A0E", fontSize: "30px" }}
 				>
 					An tâm với lựa chọn của bạn
 				</p>
@@ -213,12 +234,12 @@ export const HomePage = () => {
 			<div className="mt-20">
 				<span
 					className="ml-[120px] mt-7 font-bold"
-					style={{ color: '#562A0E', fontSize: '30px' }}
+					style={{ color: "#562A0E", fontSize: "30px" }}
 				>
 					100,000+
 				</span>
-				<span className="mt-7" style={{ color: '#562A0E', fontSize: '30px' }}>
-					{' '}
+				<span className="mt-7" style={{ color: "#562A0E", fontSize: "30px" }}>
+					{" "}
 					khách hàng sử dụng ứng dụng Antidee
 				</span>
 				<div className="flex ml-[100px] mt-4 grid grid-cols-3 ">
@@ -245,17 +266,17 @@ export const HomePage = () => {
 						<img src="/image/giupdo.png" className="w-[640px] h-[430px] ml-9" />
 					</div>
 					<div>
-						<p className="" style={{ color: '#562A0E', fontSize: '30px' }}>
+						<p className="" style={{ color: "#562A0E", fontSize: "30px" }}>
 							Chúng tôi luôn sẵn lòng giúp đỡ bạn
 						</p>
 						<span className="text-black">
-							Nếu có bất kì thắc mắc gì, bạn liên hệ với chúng tôi thông qua -{' '}
+							Nếu có bất kì thắc mắc gì, bạn liên hệ với chúng tôi thông qua -{" "}
 						</span>
 						<a
 							style={{
-								color: '#562A0E',
-								fontSize: '12px',
-								textDecoration: 'underline',
+								color: "#562A0E",
+								fontSize: "12px",
+								textDecoration: "underline",
 							}}
 							href="#"
 						>
