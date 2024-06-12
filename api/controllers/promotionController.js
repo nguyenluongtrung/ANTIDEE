69071846d03ecc4edf799294dcdae96a38844834
SilverHornn <anhkuduy1603@gmail.com>
2024-06-13 03:10:45 +0700

    fix send mail promotion

--- api/controllers/promotionController.js
+++ api/controllers/promotionController.js
@@ -1,6 +1,9 @@
 const asyncHandler = require('express-async-handler');
 const Promotion = require('./../models/promotionModel');
 const Service = require('./../models/serviceModel');
+const Account = require('../models/accountModel');
+const sendMail = require('../config/emailConfig');
+const emailTemplate = require('../utils/sampleEmailForm');
 
 const getAllPromotions = asyncHandler(async (req, res) => {
 	const promotions = await Promotion.find({
@@ -95,8 +98,26 @@ const deletePromotion = asyncHandler(async (req, res) => {
 	});
 });
 
-const createPromotion = asyncHandler(async (req, res) => {
-	const { serviceIds } = req.body;
+// const createPromotion = asyncHandler(async (req, res) => {
+// 	const { serviceIds } = req.body;
+// 	const newPromotion = await Promotion.create(req.body);
+
+// 	await Service.updateMany(
+// 		{ _id: { $in: serviceIds } },
+// 		{ $addToSet: { promotionIds: newPromotion._id } }
+// 	);
+
+// 	res.status(201).json({
+// 		status: 'success',
+// 		data: {
+// 			newPromotion,
+// 		},
+// 	});
+// });
+
+const createPromotion = async (req, res) => {
+	try {
+		const { serviceIds } = req.body;
 	const newPromotion = await Promotion.create(req.body);
 
 	await Service.updateMany(
@@ -104,13 +125,42 @@ const createPromotion = asyncHandler(async (req, res) => {
 		{ $addToSet: { promotionIds: newPromotion._id } }
 	);
 
-	res.status(201).json({
-		status: 'success',
-		data: {
-			newPromotion,
-		},
-	});
-});
+		const accounts = await Account.find({});
+
+		for (let account of accounts) {
+			if (account.role !== 'Admin') {
+				console.log('Sending email to:', account.email);
+			let email = {
+				toEmail: account.email,
+				subject: 'CHƯƠNG TRÌNH KHUYẾN MẠI ĐANG CHỜ BẠN',
+				header: 'Antidee có chương trình sale sập sàn mới dành cho bạn',
+				imageUrl:
+					'https://seotrends.com.vn/wp-content/uploads/2023/06/banner-sale-3d-1-1024x683.jpg',
+				mainContent: `
+					 
+					<p>Xin chào <span style="font-weight: bold">${account?.name}</span> Chúng tôi rất vui mừng thông báo rằng đã có một chương trình sale với ưu đãi đặc biệt dành cho bạn.</p>
+					<p>Giảm giá sốc lên tới: <strong style="color:red">${newPromotion.promotionValue}%</strong></p>
+					<p>Hãy truy cập ngay vào trang web của chúng tôi để trãi nghiệm các dịch vụ với giá chưa từng có!</p>
+					<p>Trân trọng,</p>
+					<p>Antidee Team</p>
+				`,
+			};
+			await sendMail(emailTemplate(email));
+		}
+		}
+		
+		res.status(201).json({
+			success: true,
+			data: newPromotion,
+		});
+	} catch (error) {
+		console.error('Error creating promotion or sending emails:', error);
+		res.status(400).json({
+			success: false,
+			error: error.message,
+		});
+	}
+};
 
 module.exports = {
 	getAllPromotions,
