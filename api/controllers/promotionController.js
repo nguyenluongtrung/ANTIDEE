603e0c88d739f585a873e307cd3356418ba89bd9
nguyenluongtrung <100284665+nguyenluongtrung@users.noreply.github.com>
2024-06-13 12:55:31 +0700

    Update promotion code

--- api/controllers/promotionController.js
+++ api/controllers/promotionController.js
@@ -98,57 +98,44 @@ const deletePromotion = asyncHandler(async (req, res) => {
 	});
 });
 
-// const createPromotion = asyncHandler(async (req, res) => {
-// 	const { serviceIds } = req.body;
-// 	const newPromotion = await Promotion.create(req.body);
-
-// 	await Service.updateMany(
-// 		{ _id: { $in: serviceIds } },
-// 		{ $addToSet: { promotionIds: newPromotion._id } }
-// 	);
-
-// 	res.status(201).json({
-// 		status: 'success',
-// 		data: {
-// 			newPromotion,
-// 		},
-// 	});
-// });
-
 const createPromotion = async (req, res) => {
 	try {
 		const { serviceIds } = req.body;
-	const newPromotion = await Promotion.create(req.body);
+		const newPromotion = await Promotion.create(req.body);
 
-	await Service.updateMany(
-		{ _id: { $in: serviceIds } },
-		{ $addToSet: { promotionIds: newPromotion._id } }
-	);
+		await Service.updateMany(
+			{ _id: { $in: serviceIds } },
+			{ $addToSet: { promotionIds: newPromotion._id } }
+		);
 
 		const accounts = await Account.find({});
 
 		for (let account of accounts) {
 			if (account.role !== 'Admin') {
 				console.log('Sending email to:', account.email);
-			let email = {
-				toEmail: account.email,
-				subject: 'CHƯƠNG TRÌNH KHUYẾN MẠI ĐANG CHỜ BẠN',
-				header: 'Antidee có chương trình sale sập sàn mới dành cho bạn',
-				imageUrl:
-					'https://seotrends.com.vn/wp-content/uploads/2023/06/banner-sale-3d-1-1024x683.jpg',
-				mainContent: `
+				let email = {
+					toEmail: account.email,
+					subject: 'CHƯƠNG TRÌNH KHUYẾN MẠI ĐANG CHỜ BẠN',
+					header: 'Antidee có chương trình sale sập sàn mới dành cho bạn',
+					imageUrl:
+						'https://seotrends.com.vn/wp-content/uploads/2023/06/banner-sale-3d-1-1024x683.jpg',
+					mainContent: `
 					 
-					<p>Xin chào <span style="font-weight: bold">${account?.name}</span> Chúng tôi rất vui mừng thông báo rằng đã có một chương trình sale với ưu đãi đặc biệt dành cho bạn.</p>
-					<p>Giảm giá sốc lên tới: <strong style="color:red">${newPromotion.promotionValue}%</strong></p>
+					<p>Xin chào <span style="font-weight: bold">${
+						account?.name
+					}</span> Chúng tôi rất vui mừng thông báo rằng đã có một chương trình sale với ưu đãi đặc biệt dành cho bạn.</p>
+					<p>Giảm giá sốc lên tới: <strong style="color:red">${
+						newPromotion.promotionValue * 100
+					}%</strong></p>
 					<p>Hãy truy cập ngay vào trang web của chúng tôi để trãi nghiệm các dịch vụ với giá chưa từng có!</p>
 					<p>Trân trọng,</p>
 					<p>Antidee Team</p>
 				`,
-			};
-			await sendMail(emailTemplate(email));
+				};
+				await sendMail(emailTemplate(email));
+			}
 		}
-		}
-		
+
 		res.status(201).json({
 			success: true,
 			data: newPromotion,
