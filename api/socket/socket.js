69bad262d1ae528a7074a7492bba82edb5c95c53
nguyenluongtrung <100284665+nguyenluongtrung@users.noreply.github.com>
2024-06-29 11:21:42 +0700

    Update url

--- api/socket/socket.js
+++ api/socket/socket.js
@@ -1,45 +1,47 @@
-const { Server } = require("socket.io");
+const { Server } = require('socket.io');
 
-const io = new Server({ cors: "http://localhost:5173" });
+const io = new Server({
+	cors: 'http://localhost:5173' || 'https://antidee.onrender.com',
+});
 
 let onlineUsers = [];
 
-io.on("connection", (socket) => {
-    console.log("New connection", socket.id);
+io.on('connection', (socket) => {
+	console.log('New connection', socket.id);
 
-    //listen to a connection
-    socket.on("addNewUser", (userId) => {
-        !onlineUsers.some(user => user.userId === userId) &&
-            onlineUsers.push({
-                userId,
-                socketId: socket.id,
-            })
-        console.log("onlineUsers", onlineUsers);
+	//listen to a connection
+	socket.on('addNewUser', (userId) => {
+		!onlineUsers.some((user) => user.userId === userId) &&
+			onlineUsers.push({
+				userId,
+				socketId: socket.id,
+			});
+		console.log('onlineUsers', onlineUsers);
 
-        io.emit("getOnlineUsers", onlineUsers);
-    });
+		io.emit('getOnlineUsers', onlineUsers);
+	});
 
-    //add message
-    socket.on("sendMessage", (message) => {
-        const user = onlineUsers.find((user) => user.userId === message.recipientId);
+	//add message
+	socket.on('sendMessage', (message) => {
+		const user = onlineUsers.find(
+			(user) => user.userId === message.recipientId
+		);
 
-        if (user) {
-            io.to(user.socketId).emit("getMessage", message);
-            io.to(user.socketId).emit("getNotification", {
-                senderId: message.senderId,
-                isRead: false,
-                date:new Date(),
-            });
+		if (user) {
+			io.to(user.socketId).emit('getMessage', message);
+			io.to(user.socketId).emit('getNotification', {
+				senderId: message.senderId,
+				isRead: false,
+				date: new Date(),
+			});
+		}
+	});
 
-        }
-    })
+	socket.on('disconnect', () => {
+		onlineUsers = onlineUsers.filter((user) => user.socketId !== socket.id);
 
-    socket.on("disconnect", () => {
-        onlineUsers = onlineUsers.filter(user => user.socketId !== socket.id);
-
-        io.emit("getOnlineUsers", onlineUsers);
-    })
+		io.emit('getOnlineUsers', onlineUsers);
+	});
 });
-
 
 io.listen(3000);
