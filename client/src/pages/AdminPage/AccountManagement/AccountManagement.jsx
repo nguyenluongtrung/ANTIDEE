72f1a4d8e12b730e11245775db96633bc7416229
hungdinh-dev <baohungdinhnguyen1412@gmail.com>
2024-07-04 09:48:47 +0700

    lock/unlock domestic helper

--- client/src/pages/AdminPage/AccountManagement/AccountManagement.jsx
+++ client/src/pages/AdminPage/AccountManagement/AccountManagement.jsx
@@ -1,10 +1,13 @@
-import { BiEdit, BiTrash } from "react-icons/bi";
+import { BiBlock, BiEdit, BiTrash, BiUser } from "react-icons/bi";
 import AdminSidebar from "../components/AdminSidebar/AdminSidebar";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect, useState } from "react";
 import { getAllAccounts } from "../../../features/auth/authSlice";
 import { FaLock, FaLockOpen } from "react-icons/fa";
 import { BlockAccount } from "./BlockAccount/BlockAccount";
+import { GrUserManager } from "react-icons/gr";
+import { GiConfirmed } from "react-icons/gi";
+import { FaUsersLine } from "react-icons/fa6";
 
 export const AccountManagement = () => {
   //Chỉ lấy account đã đăng nhập
@@ -13,25 +16,74 @@ export const AccountManagement = () => {
   const [isOpenBlockAccount, setIsOpenBlockAccount] = useState(false);
   const [chosenAccountId, setChosenAccountId] = useState("");
 
+  const [selectedRole, setSelectedRole] = useState(null);
+  const [blockFilter, setBlockFilter] = useState(null);
+  const [pageStanding, setPageStanding] = useState("Tất cả");
+
+  const handleRoleClick = (role) => {
+    setSelectedRole(role);
+    setBlockFilter(null); // Reset block filter when role is selected
+  };
+
+  const handleBlockFilterClick = (blockStatus) => {
+    setBlockFilter(blockStatus);
+    setSelectedRole(null); // Reset role filter when block status is selected
+  };
+
+  const handleViewAllAccounts = () => {
+    setBlockFilter(null);
+    setSelectedRole(null);
+  };
+
+  const filteredAccounts = accounts.filter((account) => {
+    const roleMatch = selectedRole ? account.role.includes(selectedRole) : true;
+    const blockMatch =
+      blockFilter !== null ? account.isBlocked === blockFilter : true;
+    return roleMatch && blockMatch;
+  });
+
+  // const handleRoleClick = (role) => {
+  //   setSelectedRole(role);
+  // };
+
+  //const filteredAccounts = selectedRole ? accounts.filter(account => account.role.includes(selectedRole)) : accounts;
+
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(getAllAccounts());
   }, []);
 
-  // const onBlockChange = async (accountId) => {
-  //   const result = await dispatch(blockAccountChange(accountId));
-  //   if (result.type.endsWith("fulfilled")) {
-  //     toast.success("Chặn người dùng thành công");
-  //   } else if (result?.error?.message === "Rejected") {
-  //     toast.error(result?.payload);
-  //   }
-  // };
   const handleGetAllAccounts = () => {
     Promise.all([dispatch(getAllAccounts())]).catch((error) => {
       console.error("Error during dispatch:", error);
     });
   };
+
+  const menu = [
+    { name: "Khách hàng", icon: <BiUser />, role: "Khách hàng" },
+    {
+      name: "Người giúp việc",
+      icon: <GrUserManager />,
+      role: "Người giúp việc",
+    },
+    { name: "Chưa chặn", icon: <GiConfirmed /> },
+    { name: "Đã chặn", icon: <BiBlock /> },
+  ];
+
+  const menuRole = [
+    { name: "Khách hàng", icon: <BiUser />, role: "Khách hàng" },
+    {
+      name: "Người giúp việc",
+      icon: <GrUserManager />,
+      role: "Người giúp việc",
+    },
+  ];
+
+  const menuBlock = [
+    { name: "Chưa chặn", icon: <GiConfirmed />, isBlocked: false },
+    { name: "Đã chặn", icon: <BiBlock />, isBlocked: true },
+  ];
 
   return (
     <div className="w-full min-h-screen bg-white flex flex-row">
@@ -43,8 +95,9 @@ export const AccountManagement = () => {
           chosenAccountId={chosenAccountId}
         />
       )}
+      {console.log("ACCCCCCC", filteredAccounts)}
       <div className="flex-1 px-10 pt-5">
-        <div className="flex items-center justify-center">
+        {/* <div className="flex items-center justify-center mb-10">
           <div className="flex-1 pt-2" style={{ paddingRight: "70%" }}>
             <span>Hiển thị </span>
             <select
@@ -57,7 +110,79 @@ export const AccountManagement = () => {
             </select>
             <span> hàng</span>
           </div>
+        </div> */}
+        <div className="flex items-center justify-between">
+          <div>
+            <h1 className="text-2xl font-semibold">Tài Khoản</h1>
+            <p className="text-sm font-medium text-gray pt-1">
+              Quản lí tất cả các tài khoản của người dùng
+            </p>
+          </div>
+
+          <div className="flex">
+            <div className="px-5 flex flex-col items-center">
+              <h2 className="text-2xl font-semibold">{accounts.length}</h2>
+              <h1 className="text-sm font-medium text-gray">Người Dùng</h1>
+            </div>
+          </div>
         </div>
+        <ul className="flex gap-x-5 items-center justify-between px-4 border-y border-gray border-opacity-50 mt-10">
+          <li className="flex flex-row items-center text-gray">
+            <button
+              className={`flex gap-x-2 items-center py-5 px-6 hover:text-primary relative group ${
+                pageStanding === "Tất cả" && "text-primary"
+              }`}
+              onClick={() => {
+                handleViewAllAccounts();
+                setPageStanding("Tất cả");
+              }}
+            >
+              <div>
+                <FaUsersLine />
+              </div>
+              <div>Tất cả</div>
+              <span className="left-3 absolute w-full h-0.5 bg-primary rounded bottom-0 scale-x-0 group-hover:scale-x-100 transition-transform ease-in-out" />
+            </button>
+          </li>
+          {menuRole.map((item, index) => {
+            return (
+              <li key={index} className="flex flex-row items-center text-gray">
+                <button
+                  className={`flex gap-x-2 items-center py-5 px-6 hover:text-primary relative group ${
+                    pageStanding === item.name && "text-primary"
+                  }`}
+                  onClick={() => {
+                    handleRoleClick(item.role);
+                    setPageStanding(item.name);
+                  }}
+                >
+                  <div>{item.icon}</div>
+                  <div>{item.name}</div>
+                  <span className="left-3 absolute w-full h-0.5 bg-primary rounded bottom-0 scale-x-0 group-hover:scale-x-100 transition-transform ease-in-out" />
+                </button>
+              </li>
+            );
+          })}
+          {menuBlock.map((item, index) => {
+            return (
+              <li key={index} className="flex flex-row items-center text-gray">
+                <button
+                  className={`flex gap-x-2 items-center py-5 px-6 hover:text-primary relative group ${
+                    pageStanding === item.name && "text-primary"
+                  }`}
+                  onClick={() => {
+                    handleBlockFilterClick(item.isBlocked);
+                    setPageStanding(item.name);
+                  }}
+                >
+                  <div>{item.icon}</div>
+                  <div>{item.name}</div>
+                  <span className="left-3 absolute w-full h-0.5 bg-primary rounded bottom-0 scale-x-0 group-hover:scale-x-100 transition-transform ease-in-out" />
+                </button>
+              </li>
+            );
+          })}
+        </ul>
         <table className="w-full border-b border-gray mt-3">
           <thead>
             <tr className="text-sm font-medium text-gray-700 border-b border-gray border-opacity-50">
@@ -71,7 +196,7 @@ export const AccountManagement = () => {
             </tr>
           </thead>
           <tbody>
-            {accounts?.map((account, index) => {
+            {filteredAccounts?.map((account, index) => {
               return (
                 <tr className="hover:bg-primary hover:bg-opacity-25 transition-colors odd:bg-light_pink  hover:cursor-pointer">
                   <td className="font-medium text-center text-gray p-3">
@@ -97,24 +222,24 @@ export const AccountManagement = () => {
                           setChosenAccountId(account._id);
                         }}
                       >
-                        {account.isBlocked ?<FaLock className="text-red m-auto"/> : <FaLockOpen className="text-primary m-auto"/>}
+                        {account.isBlocked ? (
+                          <FaLock className="text-red m-auto" />
+                        ) : (
+                          <FaLockOpen className="text-primary m-auto" />
+                        )}
                       </button>
                     </div>
                   </td>
                   <td className="">
                     <div className="flex items-center justify-center">
-                      <button className="flex items-center justify-end py-3 pr-2 text-xl group">
+                      {/* <button className="flex items-center justify-end py-3 pr-2 text-xl group">
                         <BiEdit
                           className="text-green group-hover:text-primary"
-                          // onClick={() => {
-                          //   setIsOpenUpdateQualification(true);
-                          //   setChosenQualificationId(qualification._id);
-                          // }}
                         />
-                      </button>
+                      </button> */}
                       <button className="flex items-center justify-start p-3 text-xl group">
                         <BiTrash
-                          className="text-red group-hover:text-primary"
+                          className="text-red group-hover:text-primary m-auto"
                           // onClick={() =>
                           //   handleDeleteQualification(qualification._id)
                           // }
