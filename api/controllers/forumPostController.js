8b0dfea94422d4fe13610f1e6e26caa6e5f78d72
buivangiaphatdev <buivangiaphat@gmail.com>
2024-08-13 04:57:36 +0700

    delete - get all - get by id - forum post

--- api/controllers/forumPostController.js
+++ api/controllers/forumPostController.js
@@ -1,11 +1,66 @@
-const asyncHandler = require('express-async-handler');
-const ForumPost = require('../models/forumPostModel');
+const asyncHandler = require("express-async-handler");
+const ForumPost = require("../models/forumPostModel");
 
-const createForumPost = asyncHandler(async (req, res) => {});
-const deleteForumPost = asyncHandler(async (req, res) => {});
-const updateForumPost = asyncHandler(async (req, res) => {});
-const getAllForumPosts = asyncHandler(async (req, res) => {});
-const getForumPost = asyncHandler(async (req, res) => {});
+const createForumPost = asyncHandler(async (req, res) => { });
+const deleteForumPost = asyncHandler(async (req, res) => {
+	const { forumPostId } = req.params;
+
+	// Tìm bài đăng theo ID
+	const forumPost = await ForumPost.findById(forumPostId);
+
+	// Kiểm tra nếu bài đăng không tồn tại
+	if (!forumPost) {
+		res.status(404);
+		throw new Error("Bài đăng không tồn tại");
+	}
+
+	// Xóa bài đăng
+	await ForumPost.findByIdAndDelete(req.params.forumPostId);
+
+	res.status(200).json({
+		status: "success",
+		data: {
+			id: req.params.forumPostId,
+		},
+	});
+});
+const updateForumPost = asyncHandler(async (req, res) => { });
+const getAllForumPosts = asyncHandler(async (req, res) => {
+	try {
+		const forumPosts = await ForumPost.find({});
+		res.status(200).json({
+			success: true,
+			data: { forumPosts },
+		});
+	} catch (error) {
+		res.status(500).json({
+			success: false,
+			error: error.message,
+		});
+	}
+});
+const getForumPost = asyncHandler(async (req, res) => {
+	try {
+		const { forumPostId } = req.params;
+
+		const forumPost = await ForumPost.findById(forumPostId);
+
+		if (!forumPost) {
+			res.status(404);
+			throw new Error("Bài đăng không tồn tại");
+		}
+		res.status(200).json({
+			success: true,
+			data: { forumPost },
+		})
+
+	} catch (error) {
+		res.status(500).json({
+			success: false,
+			error: error.message,
+		});
+	}
+});
 
 module.exports = {
 	createForumPost,
--- api/routes/forumPostRoutes.js
+++ api/routes/forumPostRoutes.js
@@ -1,19 +1,19 @@
-const express = require('express');
-const { protect, restrict } = require('../middleware/accountMiddleware');
+const express = require("express");
+const { protect, restrict } = require("../middleware/accountMiddleware");
 const {
-	getAllForumPosts,
-	createForumPost,
-	updateForumPost,
-	deleteForumPost,
-	getForumPost,
-} = require('../controllers/forumPostController');
+  getAllForumPosts,
+  createForumPost,
+  updateForumPost,
+  deleteForumPost,
+  getForumPost,
+} = require("../controllers/forumPostController");
 const router = express.Router();
 
-router.route('/').get(getAllForumPosts).post(createForumPost);
+router.route("/").get(getAllForumPosts).post(createForumPost);
 router
-	.route('/:forumPostId')
-	.get(getForumPost)
-	.patch(updateForumPost)
-	.delete(deleteForumPost);
+  .route("/:forumPostId")
+  .get(getForumPost)
+  .patch(updateForumPost)
+  .delete(deleteForumPost);
 
 module.exports = router;
