ce0ca0dd8eba3f255eaf11bfb788e989212b7a61
nguyenluongtrung <100284665+nguyenluongtrung@users.noreply.github.com>
2024-09-15 17:22:30 +0700

    Delete unnecessary components in forum

--- client/src/pages/ForumPage/components/TopDiscussions.jsx
+++ client/src/pages/ForumPage/components/TopDiscussions.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
+
 import { FaRegHeart, FaRegUser } from 'react-icons/fa6';
 import { HiOutlineHome } from 'react-icons/hi';
-import { IoIosAdd } from 'react-icons/io';
 
 export const TopDiscussions = () => {
 	return (
@@ -35,35 +35,6 @@ export const TopDiscussions = () => {
 							<p className=" mt-3 text-sm flex items-center gap-1">
 								1<FaRegHeart size={10} />
 							</p>
-						</div>
-					</div>
-				</div>
-				<div className={`bg-white p-4 mt-4 rounded-lg shadow-md`}>
-					<div className="flex justify-between gap-10">
-						<strong className="text-base font-bold block mb-4">
-							Lượt truy cập người dùng
-						</strong>
-						<p className="text-blue font-semibold hover:text-blue-900 cursor-pointer">
-							Tất cả
-						</p>
-					</div>
-					<div className="mb-4">
-						<div className="flex items-center mb-4">
-							<img
-								alt="avatar"
-								src="https://m.media-amazon.com/images/I/51WHgHxF5YL._AC_UF1000,1000_QL80_.jpg"
-								className="rounded-full border w-8 h-8"
-							/>
-							<div className="ml-4">
-								<p className="block font-semibold">Hung Dinh</p>
-								<p className="text-gray">#hungdinh@</p>
-							</div>
-							<div className="ml-auto flex items-center rounded-full border border-blue text-blue hover:bg-blue hover:text-white">
-								<IoIosAdd className=" text-2xl ml-1" />
-								<button className=" text-sm font-semibold py-1 px-2">
-									Theo dõi
-								</button>
-							</div>
 						</div>
 					</div>
 				</div>
