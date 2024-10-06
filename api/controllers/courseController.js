5089538d8738c3b2a316dd735d8abefad0bf4ac1
nguyenluongtrung <100284665+nguyenluongtrung@users.noreply.github.com>
2024-10-06 10:24:06 +0700

    Update code

--- api/controllers/courseController.js
+++ api/controllers/courseController.js
@@ -1,10 +1,8 @@
-const asyncHandler = require("express-async-handler");
-
-const Account = require('../models/accountModel');
+const asyncHandler = require('express-async-handler');
 const Course = require('../models/courseModel');
 
 const getAllCourses = asyncHandler(async (req, res) => {
-    const courses = await Course.find({})
+	const courses = await Course.find({});
 
 	res.status(200).json({
 		status: 'success',
@@ -14,71 +12,71 @@ const getAllCourses = asyncHandler(async (req, res) => {
 	});
 });
 const getCourse = asyncHandler(async (req, res) => {
-    const course = await Course.findById(req.params.courseId);
+	const course = await Course.findById(req.params.courseId);
 
-    if (!course) {
-      res.status(404);
-      throw new Error("Không tìm thấy khóa học");
-    }
-  
-    res.status(200).json({
-      status: "success",
-      data: {
-        course,
-      },
-    });
+	if (!course) {
+		res.status(404);
+		throw new Error('Không tìm thấy khóa học');
+	}
+
+	res.status(200).json({
+		status: 'success',
+		data: {
+			course,
+		},
+	});
 });
 const createCourse = asyncHandler(async (req, res) => {
-    const course = await Course.create(req.body);
+	const course = await Course.create(req.body);
 
-    res.status(201).json({
-      status: "success",
-      data: {
-        video,
-      },
-    });
+	res.status(201).json({
+		status: 'success',
+		data: {
+			course,
+		},
+	});
 });
 const updateCourse = asyncHandler(async (req, res) => {
-    const course = await Course.findById(req.params.courseId);
+	const course = await Course.findById(req.params.courseId);
 
-  if (!course) {
-    res.status(404);
-    throw new Error("Không tìm thấy khóa học");
-  }
+	if (!course) {
+		res.status(404);
+		throw new Error('Không tìm thấy khóa học');
+	}
 
-  const updatedCourse = await Course.findByIdAndUpdate(
-    req.params.courseId,
-    req.body,
-    { new: true }
-  );
+	const updatedCourse = await Course.findByIdAndUpdate(
+		req.params.courseId,
+		req.body,
+		{ new: true }
+	);
 
-  res.status(200).json({
-    status: "success",
-    data: {
-      updatedCourse,
-    },
-  });
+	res.status(200).json({
+		status: 'success',
+		data: {
+			updatedCourse,
+		},
+	});
 });
 const deleteCourse = asyncHandler(async (req, res) => {
-    const course = await Course.findById(req.params.courseId);
+	const course = await Course.findById(req.params.courseId);
 
-  if (!course) {
-    res.status(404);
-    throw new Error("Không tìm thấy khóa học");
-  }
+	if (!course) {
+		res.status(404);
+		throw new Error('Không tìm thấy khóa học');
+	}
 
-  await Course.findByIdAndDelete(req.params.courseId);
+	await Course.findByIdAndDelete(req.params.courseId);
 
-  res.status(200).json({
-    status: "success",
-    data: { id: req.params.courseId },
-  });
+	res.status(200).json({
+		status: 'success',
+		data: { id: req.params.courseId },
+	});
 });
 
 module.exports = {
-getAllCourses,
-getCourse,
-createCourse,
-updateCourse,
-deleteCourse,
+	getAllCourses,
+	getCourse,
+	createCourse,
+	updateCourse,
+	deleteCourse,
 };
