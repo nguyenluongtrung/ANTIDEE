40bf9e5223cd9467ef8c5036cc8b083cec439cc1
buivangiaphatdev <buivangiaphat@gmail.com>
2024-10-27 17:31:57 +0700

    fix UI pagination

--- client/src/pages/MyAccount/JobPostingHistory/JobPostingHistory.jsx
+++ client/src/pages/MyAccount/JobPostingHistory/JobPostingHistory.jsx
@@ -13,6 +13,7 @@ import {
 } from '../../../utils/format';
 import { HistoryJobPostDetail } from './HistoryJobPostDetail/HistoryJobPostDetail';
 import { calculateTotalPages, getPageItems, nextPage, previousPage } from '../../../utils/pagination';
+import Pagination from '../../../components/Pagination/Pagination';
 
 export const JobPostingHistory = () => {
 	const [myAccountId, setMyAccountId] = useState();
@@ -115,7 +116,7 @@ export const JobPostingHistory = () => {
 
 	const totalPages = calculateTotalPages(myJobHistory.length, rowsPerPage);
 	const selectedJobs = getPageItems(myJobHistory, currentPage, rowsPerPage);
-  
+
 	const handleNextPage = () => setCurrentPage(nextPage(currentPage, totalPages));
 	const handlePreviousPage = () => setCurrentPage(previousPage(currentPage));
 
@@ -319,22 +320,26 @@ export const JobPostingHistory = () => {
 					</div>
 				)}
 
-				<div className="flex justify-center items-center mt-4 space-x-2">
-					<button
-						className="bg-light_gray hover:bg-gray hover:text-white w-fit px-4 py-2 rounded disabled:opacity-50"
-						disabled={currentPage === 1}
-						onClick={handlePreviousPage}
-					>
-						&#9664;
-					</button>
-					<span className="text-sm font-semibold">Page {currentPage} of {totalPages}</span>
-					<button
-						className="bg-light_gray hover:bg-gray hover:text-white w-fit px-4 py-2 rounded disabled:opacity-50"
-						disabled={currentPage === totalPages}
-						onClick={handleNextPage}
-					>
-						&#9654;
-					</button>
+				<div className="flex items-center justify-between border-t border-gray bg-white px-4 py-3 sm:px-6">
+					<div className="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
+						<div>
+							<p className="text-sm text-gray">
+								Hiển thị <span className="font-medium">{(currentPage - 1) * rowsPerPage + 1}</span> đến{' '}
+								<span className="font-medium">
+									{Math.min(currentPage * rowsPerPage, myJobHistory.length)}
+								</span>{' '}
+								trong <span className="font-medium">{myJobHistory.length}</span> kết quả
+							</p>
+						</div>
+						<div>
+							<Pagination totalPages={totalPages}
+								currentPage={currentPage}
+								onPageChange={(page) => setCurrentPage(page)}
+								onNextPage={handleNextPage}
+								onPreviousPage={handlePreviousPage}
+								rowsPerPage={rowsPerPage} />
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
